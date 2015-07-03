9388fb6dc407d51f934fa662c528304da404c5cb
Donald Kelsey <dmkelseyii@gmail.com>
2015-07-02 19:59:03 -0500

    basic tests, basic functionality.

--- lib/tetris/tetris.rb
+++ lib/tetris/tetris.rb
@@ -25,52 +25,103 @@ Board
 
 =end
 
+# Maintains state of board.
 class Board
 
+  BOARD_WIDTH = 10
+  BOARD_HEIGHT = 15
+
+  attr_accessor :state
+
   def initialize
-    @game_board = []
-    @block_falling = false
-    @falling_x = 0
-    @falling_y = 0
-    @block_shape = []
-    create_board
+    @state = []
+    @state = initialize_board_state
+    @current_block = nil
   end
 
-  def create_board
-    10.times do 
-      @game_board << [0, 0, 0, 0, 0, 0]
+  def initialize_board_state
+    state = []
+    BOARD_HEIGHT.times do
+      state << Array.new(BOARD_WIDTH) {|column| column = 0}
     end
+    return state
   end
 
   def add_block(block)
-    #check if block exists at start - collision - cause game over
-    @block_shape = block.shape
-    @block_falling = true
-    @falling_x = 0
-    @falling_y = 0
+    @current_block = block
+  end
 
-    block.shape.each do |coords|
-      @game_board[coords[0]][coords[1]] = "x"
+  def place_block
+    if @current_block
+      @current_block.current_position.each do |pos|
+        @state[pos[1]][pos[0]] = 1
+      end
+    end
+    @current_block = nil
+  end
+
+
+  # Checks if position moving to is out of bounds or full.
+  def full?(x, y)
+    x >= BOARD_WIDTH || x < 0 || y >= BOARD_HEIGHT || (@state[x][y] == 1)
+  end
+
+  def render
+    temp_add_current_block
+    (BOARD_HEIGHT-1).downto(0) do |y|
+      p @state[y]
+    end
+    temp_remove_current_block
+  end
+
+  def temp_add_current_block
+    if @current_block
+      @current_block.current_position.each do |pos|
+        @state[pos[1]][pos[0]] = 1
+      end
     end
   end
 
-  def block_fall
-    
-    @block_shape.each do |coords|
-      @game_board[coords[0] + @falling_y][coords[1] + @falling_x] = 0
-    end
-
-    @falling_y += 1
-    @block_shape.each do |coords|
-      @game_board[coords[0] + @falling_y][coords[1] + @falling_x] = "x"
+  def temp_remove_current_block
+    if @current_block
+      @current_block.current_position.each do |pos|
+        @state[pos[1]][pos[0]] = 0
+      end
     end
   end
 
 end
 
+# Maintains state of individual block.
 class Block
 
-  attr_reader :shape
+  attr_reader :shape, :x_pos, :y_pos
+
+  def initialize(board)
+    @board = board
+    @x_pos = 0
+    @y_pos = 0
+    @shape = [[0,0],[0,1],[1,0],[1,1]]
+  end
+
+  # Returns each individual square of the shape.
+
+  def move(x_offset, y_offset)
+    unless collides?(x_offset, y_offset)
+      @x_pos += x_offset
+      @y_pos += y_offset
+    end
+  end
+
+  # This checks if there is a collision/out of bounds
+  # by looking at the squares that we're trying to move to.
+  def collides?(x_dir, y_dir)
+    @shape.any? {|square| @board.full?(square[0] + @x_pos + x_dir, square[1] + @y_pos + y_dir)}
+  end
+
+  def current_position
+    @shape.map{|square| [square[0] + @x_pos, square[1] + @y_pos]}
+  end
 
 
 
@@ -84,10 +135,21 @@ class SquareBlock < Block
   end
 
 end
-
-
-
 =begin
+board = Board.new
+block = Block.new(board)
+
+board.add_block(block)
+board.render
+
+#board.move_current_block(0,-1)
+
+
+board.render
+
+
+
+
 
   [.,.,.,.,.,.]
   [.,.,.,.,.,.]
---
+++ spec/tetris/tetris_spec.rb
@@ -0,0 +1,62 @@
+require 'tetris/tetris'
+
+describe Board do
+
+  let(:board) {Board.new}
+  let(:block) {Block.new(board)}
+
+  it 'should properly add block' do
+    board.add_block(block)
+    expect(board.instance_variable_get(:@current_block)).not_to be_nil
+  end
+
+end
+
+describe Block do
+  let(:board) {Board.new}
+  let(:block) {Block.new(board)}
+
+  describe "#move" do
+
+    it 'should properly let block move' do
+      block.move(0,1)
+      expect(block.current_position).to eq([[0,1],[0,2],[1,1],[1,2]])
+    end
+
+    it 'should not move if there is a block in the way' do
+      board.state[0][3] = 1
+      board.state[0][4] = 1
+      board.state[1][3] = 1
+      board.state[1][4] = 1
+
+      block.move(0,1)
+      expect(block.current_position).to eq([[0,1],[0,2],[1,1],[1,2]])
+
+      block.move(0,1)
+      expect(block.current_position).to eq([[0,1],[0,2],[1,1],[1,2]])
+    end
+
+    context 'bounds' do
+      it "should not go out of bounds diagonally" do
+        999.times do
+          block.move(1,1)
+        end
+        expect(block.current_position).to eq([[8,8],[8,9],[9,8],[9,9]])
+      end
+
+      it "should not go out of bounds vertically" do
+        999.times do
+          block.move(0,1)
+        end
+        expect(block.current_position).to eq([[0,13],[0,14],[1,13],[1,14]])
+      end
+
+      it "should not go out of bounds horizontally" do
+        999.times do
+          block.move(1,0)
+        end
+        expect(block.current_position).to eq([[8,0],[8,1],[9,0],[9,1]])
+      end
+    end
+  end
+end
